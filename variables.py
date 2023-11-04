53e2e0b103ef44f0da2599c58d18aee52f5fd032
MarisKX <maris.kurpnieks@gmail.com>
2023-11-04 23:34:54 +0100

    Variables

---
+++ variables.py
@@ -0,0 +1,10 @@
+import keyword
+
+name = "Bread"  # Variables starts with lowercase letter - 1name is a wrong name for variable
+product_description = "Rye bread"
+price = 10
+in_stock = True
+
+print(name, product_description, price, in_stock)
+print(type(in_stock))
+print(keyword.kwlist)
