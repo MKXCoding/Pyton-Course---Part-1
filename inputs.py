ef614c64f29a709f54c1055dab6d85a91fc29f1c
MarisKX <maris.kurpnieks@gmail.com>
2023-11-04 23:46:12 +0100

    Inputs

---
+++ inputs.py
@@ -0,0 +1,11 @@
+name = input("Enter Your name: ")
+print("Hello " + name)
+
+# Excercise:
+"""Create 2 inputes, that produdes the output - Hello, Mars! You are from Netherlands."""
+
+# Solution:
+name = input("Enter Your name: ")
+country = input("Enter Your country: ")
+
+print("Hello " + name + "! You are from " + country + ".")
